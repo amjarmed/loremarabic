dde5746cf8888032caee148b0a06de3b6ab305b5
amjarino <mohamedamjar8@gmail.com>
2019-04-20 18:51:18 +0100

    commit all

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-/*
- * @title: loremarabic
+/**
+ * @name: `loremarabic`
  *@desc: a tool to generate arabic lorem text
  *@author : amjarino
  */
-
-const opts = {
-  para: "لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم فينايم,كيواس نوستريد أكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايت نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت ان كيولبا كيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم.",
-}
-
-function ar_para(max = 1) {
-  let data = "";
-
-  if (typeof max === "number" && max > 0) {
-    for (let i = 0; i < max; i++) data += ` \r\n ${opts.para}`;
-    return data;
-  } else {
-    console.log("error : pass only numbers and greater than 0 !");
-    return false;
-  }
-}
-
-function ar_sent(length = 70) {
-  let data = '';
-  if (typeof length === "number" && length > 0) {
-    data = opts.para.slice(0, length) + ". \n";
-    return data;
-  } else {
-    console.log("error : pass only numbers and greater than 0 !");
-    return false;
-  }
-}
-
-// export functions
-
 module.exports = {
-  ar_para,
-  ar_sent
+  'arLetter': require("./src/methods/ar-letter"),
+  'arWord': require("./src/methods/ar-word"),
+  'arSentence': require("./src/methods/ar-sentence"),
+  'arParagraph': require("./src/methods/ar-paragraph"),
+  'arWordsPara': require("./src/methods/ar-words-para"),
+  'loremEnglish': require("./src/methods/lorem-english"),
+  'loremOriginal': require("./src/methods/lorem-original"),
+  'arPosts': require("./src/methods/ar-posts")
 }
--- player.js
+++
@@ -1,9 +0,0 @@
-// playground for testing 
-
-const loremar = require('./index');
-
-
-console.log(loremar.ar_para(3));
-console.log("==============================");
-
-console.log(loremar.ar_sent(30));
